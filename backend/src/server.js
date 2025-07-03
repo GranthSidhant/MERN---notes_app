1332dbd6ffd8fa380ea25f7b764299923306bcb5
Granth_Sidhant <sidhant2granth@gmail.com>
2025-07-04 01:30:57 +0530

    in src folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
  import express from "express";
 // const express = require("express");
 
-import notesRoutes from "./routes/notesRoutes"
+import notesRoutes from "./routes/notesRoutes.js"
+
 
 const app = express();
 
-app.use = ("/api/notes", notesRoutes);
-
+app.use("/api/notes", notesRoutes);
 
 app.listen(5001,() => {
     console.log(`server is running on port 5001`);
